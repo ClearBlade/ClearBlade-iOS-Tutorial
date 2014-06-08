a02f977091653089aa6fd0c7eb823dba66b38584
Michael Sprague <msprague@clearblade.com>
2014-06-08 16:37:12 -0700

    initial project commit

---
+++ ClearIO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClearIO
+//
+//  Created by Michael on 6/8/14.
+//  Copyright (c) 2014 ClearBlade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
